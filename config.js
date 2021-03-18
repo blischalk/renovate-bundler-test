055fa9c4261e7d285f82951f9cccd3ceb52bb29d
Brett Lischalk <blischalk@brettlischalk.com>
2021-03-18 16:32:22 -0500

    Add config.js to illustrate config being used

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  autodiscover: false,
+  logLevel: "debug",
+  platform: "github",
+  gitAuthor: "GitHub Bot <noreply@somedomain.com>",
+  branchPrefix: "renovate-fix-",
+  commitMessagePrefix: "[ISSUE-NUMBER] ",
+  onboardingBranch: "renovation-onboarding",
+  enabledManagers: ["bundler", "dockerfile", "npm"],
+  repositories: [
+    {repository: 'blischalk/renovate-bundler-test', rangeStrategy: 'update-lockfile'}
+  ]
+}
